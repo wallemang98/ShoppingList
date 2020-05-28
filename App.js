c7cce623e9327d3a2d78aa72a52c9925070b000b
wallemang98 <wallemang98@gmail.com>
2020-05-28 01:46:59 -0400

    Working Shopping List application

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
-
+import AddItem from './components/AddItem';
 //React native components
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
 
 import { v4 as uuidv4 } from 'uuid';
 
@@ -20,13 +20,26 @@ const App = () => {
         setItems(prevItems => {
             return prevItems.filter(item => item.id != id);
         });
-    }
+    };
+
+    const addItem = text => {
+        //if nothing is typed in
+        if (!text) {
+            //'title', 'message', object with text for button
+            Alert.alert('Error', 'Please enter an item', [{ text: 'Ok' }]);
+        } else {
+            setItems(prevItems => {
+                return [{ id: uuidv4(), text }, ...prevItems]
+            });
+        }
+    };
 
     return (
         //Most fundamental component for building UI, styled using Flexbox
         <View style={ styles.container }>
             <Header />
 
+            <AddItem addItem={addItem} />
             <FlatList
                 data={items}
                 renderItem={({ item }) => <ListItem item={item} deleteItem={deleteItem}/>}
